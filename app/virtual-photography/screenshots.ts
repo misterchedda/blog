2132bea45a5112622699173581f0108d53ba76ff
MisterChedda <mister.chedda.here@gmail.com>
2026-01-04 23:27:25 +0530

    add new pics

--- app/virtual-photography/screenshots.ts
+++ app/virtual-photography/screenshots.ts
@@ -13,9 +13,32 @@ export interface Screenshot {
 // Thumbnails should be ~400px wide, ~50KB max
 // Full images can be original resolution
 export const screenshots: Screenshot[] = [
-
-
-
+  { 
+    id: 'cyberpunk_000',
+    game: 'Cyberpunk 2077',
+    thumb: '/screenshots/thumbs/cyberpunk_000.png',
+    full: '/screenshots/full/cyberpunk_000.png',
+    comment: 'V(isi)',
+  },
+  { 
+    id: 'cyberpunk_001',
+    game: 'Cyberpunk 2077',
+    thumb: '/screenshots/thumbs/cyberpunk_001.png',
+    full: '/screenshots/full/cyberpunk_001.png',
+  },
+  { 
+    id: 'cyberpunk_002',
+    game: 'Cyberpunk 2077',
+    thumb: '/screenshots/thumbs/cyberpunk_002.png',
+    full: '/screenshots/full/cyberpunk_002.png',
+  },
+    { 
+    id: 'cyberpunk_005',
+    game: 'Cyberpunk 2077',
+    thumb: '/screenshots/thumbs/cyberpunk_005.png',
+    full: '/screenshots/full/cyberpunk_005.png',
+    anchor: true,
+  },
   { 
     id: 'cyberpunk_003',
     game: 'Cyberpunk 2077',
@@ -23,58 +46,36 @@ export const screenshots: Screenshot[] = [
     full: '/screenshots/full/cyberpunk_003.png',
   },
   { 
-    id: 'cyberpunk_019',
+    id: 'cyberpunk_004',
     game: 'Cyberpunk 2077',
-    thumb: '/screenshots/thumbs/cyberpunk_019.png',
-    full: '/screenshots/full/cyberpunk_019.png',
-    anchor: true,
-  },
-  { 
-    id: 'cyberpunk_005',
-    game: 'Cyberpunk 2077',
-    thumb: '/screenshots/thumbs/cyberpunk_005.png',
-    full: '/screenshots/full/cyberpunk_005.png',
+    thumb: '/screenshots/thumbs/cyberpunk_004.png',
+    full: '/screenshots/full/cyberpunk_004.png',
   },
 
   { 
-    id: 'cyberpunk_022',
+    id: 'cyberpunk_006',
     game: 'Cyberpunk 2077',
-    thumb: '/screenshots/thumbs/cyberpunk_022.png',
-    full: '/screenshots/full/cyberpunk_022.png',
+    thumb: '/screenshots/thumbs/cyberpunk_006.png',
+    full: '/screenshots/full/cyberpunk_006.png',
   },
+  {
+  id: 'cyberpunk_69',
+  game: 'Cyberpunk 2077',
+  thumb: '/screenshots/thumbs/cyberpunk_69.png',
+  full: '/screenshots/full/cyberpunk_69.png',
+  anchor: true,
+},
   { 
-    id: 'cyberpunk_008',
+    id: 'cyberpunk_007',
     game: 'Cyberpunk 2077',
-    thumb: '/screenshots/thumbs/cyberpunk_008.png',
-    full: '/screenshots/full/cyberpunk_008.png',
-    comment: 'Still from my mod "TPP Rooftop Hangout" (Custom quest - "One More Hour")',
+    thumb: '/screenshots/thumbs/cyberpunk_007.png',
+    full: '/screenshots/full/cyberpunk_007.png',
   },
   { 
     id: 'cyberpunk_009',
     game: 'Cyberpunk 2077',
     thumb: '/screenshots/thumbs/cyberpunk_009.png',
     full: '/screenshots/full/cyberpunk_009.png',
-    comment: 'This was while I was making an unreleased Evangelion-inspired mod',
-  },
-    { 
-    id: 'cyberpunk_017',
-    game: 'Cyberpunk 2077',
-    thumb: '/screenshots/thumbs/cyberpunk_017.png',
-    full: '/screenshots/full/cyberpunk_017.png',
-    anchor: true,
-  },
-    { 
-    id: 'cyberpunk_010',
-    game: 'Cyberpunk 2077',
-    thumb: '/screenshots/thumbs/cyberpunk_010.png',
-    full: '/screenshots/full/cyberpunk_010.png',
-    comment: 'This was while I was making an unreleased Evangelion-inspired mod',
-  },
-    { 
-    id: 'cyberpunk_001',
-    game: 'Cyberpunk 2077',
-    thumb: '/screenshots/thumbs/cyberpunk_001.png',
-    full: '/screenshots/full/cyberpunk_001.png',
   },
   { 
     id: 'cyberpunk_011',
@@ -83,34 +84,18 @@ export const screenshots: Screenshot[] = [
     full: '/screenshots/full/cyberpunk_011.png',
   },
   { 
-    id: 'cyberpunk_012',
-    game: 'Cyberpunk 2077',
-    thumb: '/screenshots/thumbs/cyberpunk_012.png',
-    full: '/screenshots/full/cyberpunk_012.png',
-  },
-  { 
     id: 'cyberpunk_013',
     game: 'Cyberpunk 2077',
     thumb: '/screenshots/thumbs/cyberpunk_013.png',
     full: '/screenshots/full/cyberpunk_013.png',
-  },
-      { 
-    id: 'cyberpunk_007',
-    game: 'Cyberpunk 2077',
-    thumb: '/screenshots/thumbs/cyberpunk_007.png',
-    full: '/screenshots/full/cyberpunk_007.png',
   },
   { 
     id: 'cyberpunk_014',
     game: 'Cyberpunk 2077',
     thumb: '/screenshots/thumbs/cyberpunk_014.png',
     full: '/screenshots/full/cyberpunk_014.png',
-  },
-    { 
-    id: 'cyberpunk_002',
-    game: 'Cyberpunk 2077',
-    thumb: '/screenshots/thumbs/cyberpunk_002.png',
-    full: '/screenshots/full/cyberpunk_002.png',
+    comment: 'Still from my mod "TPP Rooftop Hangout" (Custom quest - "One More Hour")',
+    anchor: true,
   },
   { 
     id: 'cyberpunk_015',
@@ -123,56 +108,84 @@ export const screenshots: Screenshot[] = [
     game: 'Cyberpunk 2077',
     thumb: '/screenshots/thumbs/cyberpunk_016.png',
     full: '/screenshots/full/cyberpunk_016.png',
-  },
-
-  { 
-    id: 'cyberpunk_004',
-    game: 'Cyberpunk 2077',
-    thumb: '/screenshots/thumbs/cyberpunk_004.png',
-    full: '/screenshots/full/cyberpunk_004.png',
-  },
-    { 
-    id: 'cyberpunk_021',
-    game: 'Cyberpunk 2077',
-    thumb: '/screenshots/thumbs/cyberpunk_021.png',
-    full: '/screenshots/full/cyberpunk_021.png',
-  },
-    {
-    id: 'metro2033-001',
-    game: 'Metro 2033',
-    thumb: '/screenshots/thumbs/metro2033_001.jpg',
-    full: '/screenshots/full/metro2033_001.jpg',
-  },
-    { 
-    id: 'cyberpunk_006',
-    game: 'Cyberpunk 2077',
-    thumb: '/screenshots/thumbs/cyberpunk_006.png',
-    full: '/screenshots/full/cyberpunk_006.png',
     comment: 'Still from my mod "TPP Rooftop Hangout" (Custom quest - "One More Hour")',
-    anchor: true,
+  },
+  { 
+    id: 'cyberpunk_017',
+    game: 'Cyberpunk 2077',
+    thumb: '/screenshots/thumbs/cyberpunk_017.png',
+    full: '/screenshots/full/cyberpunk_017.png',
+    comment: 'This was while I was making an unreleased Evangelion-inspired mod',
   },
   { 
     id: 'cyberpunk_018',
     game: 'Cyberpunk 2077',
     thumb: '/screenshots/thumbs/cyberpunk_018.png',
     full: '/screenshots/full/cyberpunk_018.png',
+    comment: 'This was while I was making an unreleased Evangelion-inspired mod',
   },
-
+  { 
+    id: 'cyberpunk_019',
+    game: 'Cyberpunk 2077',
+    thumb: '/screenshots/thumbs/cyberpunk_019.png',
+    full: '/screenshots/full/cyberpunk_019.png',
+  },
   { 
     id: 'cyberpunk_020',
     game: 'Cyberpunk 2077',
     thumb: '/screenshots/thumbs/cyberpunk_020.png',
     full: '/screenshots/full/cyberpunk_020.png',
   },
-  
-
-  {
-    id: 'metro2033-002',
-    game: 'Metro 2033',
-    thumb: '/screenshots/thumbs/metro2033_002.jpg',
-    full: '/screenshots/full/metro2033_002.jpg',
+  { 
+    id: 'cyberpunk_021',
+    game: 'Cyberpunk 2077',
+    thumb: '/screenshots/thumbs/cyberpunk_021.png',
+    full: '/screenshots/full/cyberpunk_021.png',
+  },
+  { 
+    id: 'cyberpunk_022',
+    game: 'Cyberpunk 2077',
+    thumb: '/screenshots/thumbs/cyberpunk_022.png',
+    full: '/screenshots/full/cyberpunk_022.png',
   },
 
+  { 
+    id: 'cyberpunk_024',
+    game: 'Cyberpunk 2077',
+    thumb: '/screenshots/thumbs/cyberpunk_024.png',
+    full: '/screenshots/full/cyberpunk_024.png',
+  },
+  { 
+    id: 'cyberpunk_025',
+    game: 'Cyberpunk 2077',
+    thumb: '/screenshots/thumbs/cyberpunk_025.png',
+    full: '/screenshots/full/cyberpunk_025.png',
+  },
+  { 
+    id: 'cyberpunk_026',
+    game: 'Cyberpunk 2077',
+    thumb: '/screenshots/thumbs/cyberpunk_026.png',
+    full: '/screenshots/full/cyberpunk_026.png',
+  },
+  { 
+    id: 'cyberpunk_027',
+    game: 'Cyberpunk 2077',
+    thumb: '/screenshots/thumbs/cyberpunk_027.png',
+    full: '/screenshots/full/cyberpunk_027.png',
+    anchor: true,
+  },
+  { 
+    id: 'cyberpunk_028',
+    game: 'Cyberpunk 2077',
+    thumb: '/screenshots/thumbs/cyberpunk_028.png',
+    full: '/screenshots/full/cyberpunk_028.png',
+  },
+  { 
+    id: 'cyberpunk_029',
+    game: 'Cyberpunk 2077',
+    thumb: '/screenshots/thumbs/cyberpunk_029.png',
+    full: '/screenshots/full/cyberpunk_029.png',
+  },
 
 ]
 
